7597b114039d3aca9a0de06cf41c85d49cac50dd
endsley <juliusctw@gmail.com>
2019-03-20 08:47:14 -0400

    slight update

--- get_k_nearest_samples.py
+++ get_k_nearest_samples.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import euclidean_distances
 # Return the id of k nearest sample points
 
 
@@ -9,8 +10,14 @@ samples = [[0, 0, 2], [1, 0, 1], [0, 0, 1], [1,1,1]]
 
 neigh = NearestNeighbors(2, 0.4)
 neigh.fit(samples)
-result = neigh.kneighbors([[1, 1, 1.3]], 2, return_distance=False)
+[distances, indices] = neigh.kneighbors([[1, 1, 1.3]], 2, return_distance=True)
 
-print(result)
 
+
+print(distances)
+print(indices)
+
+
+d3 = euclidean_distances(np.array([[1,1,1]]), np.array([[1, 1, 1.3]]))
+print(d3)
 
